70f927cc8865fd60171863187751520530f2e2ff
Jennings Inge <jennings_inge@utexas.edu>
2026-08-13 15:12:37 -0500

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.java
@@ -0,0 +1,33 @@
+class MinStack {
+
+    private List<Integer> stack;
+
+    private List<Integer> minElStack;
+
+    public MinStack() {
+        stack = new ArrayList<Integer>();
+        minElStack = new ArrayList<Integer>();
+    }
+    
+    public void push(int val) {
+        if (stack.size() == 0 || val <= minElStack.get(minElStack.size()-1)) {
+            minElStack.add(val);
+        }
+        stack.add(val);
+    }
+    
+    public void pop() {
+        Integer val = stack.remove(stack.size()-1);
+        if (val.equals(minElStack.get(minElStack.size()-1))) {
+            minElStack.remove(minElStack.size()-1);
+        }
+    }
+    
+    public int top() {
+        return stack.get(stack.size()-1);
+    }
+    
+    public int getMin() {
+        return minElStack.get(minElStack.size()-1);
+    }
+}
